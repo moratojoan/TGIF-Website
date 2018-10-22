309d501729a0d000ff1163f0bdbec048e3f8a112
moratojoan <moratojoan@gmail.com>
2018-10-22 17:42:58 +0200

    2018-10-22

--- scripts/main.js
+++ scripts/main.js
@@ -2,110 +2,143 @@
 /* eslint "no-console": "off"  */
 /* global$ */
 
-//------------------MAIN----------------------MAIN-----------------------------MAIN--------------
-//------------------MAIN----------------------MAIN-----------------------------MAIN--------------
-//------------------MAIN----------------------MAIN-----------------------------MAIN--------------
+var path = window.location.pathname;
+var page = path.split("/").pop();
+var fileName = location.pathname.split("/").slice(-1)[0];
+console.log(path, page, fileName);
 
-//START: VAR GLOBALS
 var page = document.querySelector('[data-page]').dataset.page;
-//FINISH: VAR GLOBALS
-
-//START: GET DATA
-if (page == "senate-data" || page == "house-data" || page == "senate-attendance" || page == "senate-party-loyalty" || page == "house-party-loyalty" || page == "house-attendance") {
-    var data = data;
-    var membersArray = data.results[0].members;
-}
-//FINISH: GET DATA
-
-//START PAGE: INDEX
+var url;
 if (page == "home") {
+    homeMain();
+} else if (page == "senate-data" || page == "senate-attendance" || page == "senate-party-loyalty") {
+    url = "https://api.propublica.org/congress/v1/113/senate/members.json";
+    start_fetch(url);
+} else if (page == "house-data" || page == "house-party-loyalty" || page == "house-attendance") {
+    url = "https://api.propublica.org/congress/v1/113/house/members.json";
+    start_fetch(url);
+}
+
+//AJAX
+function start_fetch(url) {
+    fetch(url, {
+            method: "GET",
+            headers: {
+                "X-API-Key": "Mav6sG8mZwoWpVesShC3VKYBZtpLKchoGj4UZI49"
+            }
+        })
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(function (myData) {
+            //this is the only place we know we have the data.
+            members = myData.results[0].members;
+            main(members);
+            document.getElementsByClassName("lds-spinner")[0].style.display = "none";
+            document.getElementsByClassName("loadContent")[0].style.display = "block";
+        })
+        .catch(function (error) {
+            console.log("Request failed: " + error.message);
+        })
+}
+
+
+//------------------MAIN----------------------MAIN-----------------------------MAIN--------------
+//------------------MAIN----------------------MAIN-----------------------------MAIN--------------
+//------------------MAIN----------------------MAIN-----------------------------MAIN--------------
+function homeMain() {
+    //START PAGE: INDEX
     var a = document.getElementsByClassName("colapseMoreLess");
     for (let i = 0; i < a.length; i++) {
         a[i].onclick = function () {
             moreLess(i)
         }
     }
+    //FINISH PAGE: INDEX
 }
-//FINISH PAGE: INDEX
 
-//START PAGES: SENATE-DATA, HOUSE-DATA
-if (page == "senate-data" || page == "house-data") {
-    //Filters by: CheckBoxes, DropDown
-    var checkBox = document.getElementsByTagName("input");
-    var select = document.getElementsByTagName("select");
+function main(membersArray) {
+    //START PAGES: SENATE-DATA, HOUSE-DATA
+    if (page == "senate-data" || page == "house-data") {
+        //Filters by: CheckBoxes, DropDown
+        var checkBox = document.getElementsByTagName("input");
+        var select = document.getElementsByTagName("select");
 
-    for (let i = 0; i < checkBox.length; i++) {
-        checkBox[i].onchange = function () {
+        for (let i = 0; i < checkBox.length; i++) {
+            checkBox[i].onchange = function () {
+                createMembersTable(membersArray, checkBox, select)
+            }
+        }
+        select[0].onchange = function () {
             createMembersTable(membersArray, checkBox, select)
         }
-    }
-    select[0].onchange = function () {
-        createMembersTable(membersArray, checkBox, select)
-    }
 
-    //Create Table, Create DropDownMenu
-    createMembersTable(membersArray, checkBox, select);
-    createDropdownMenu(membersArray);
-}
-//FINISH PAGES: SENATE-DATA, HOUSE-DATA
+        //Create Table, Create DropDownMenu
+        createMembersTable(membersArray, checkBox, select);
+        createDropdownMenu(membersArray);
+    }
+    //FINISH PAGES: SENATE-DATA, HOUSE-DATA
 
-//START PAGES: SENATE/HOUSE-ATTENDANCE, SENATE/HOUSE-PARTY-LOYALTY
-if (page == "senate-attendance" || page == "senate-party-loyalty" || page == "house-attendance" || page == "house-party-loyalty") {
-    //Create an array to each Party,
-    var democratsArray = [];
-    var republicantsArray = [];
-    var independentsArray = [];
-    for (let i = 0; i < membersArray.length; i++) {
-        switch (membersArray[i].party) {
-            case "D":
-                democratsArray.push(membersArray[i]);
-                break;
-            case "R":
-                republicantsArray.push(membersArray[i]);
-                break;
-            case "I":
-                independentsArray.push(membersArray[i]);
-                break;
+    //START PAGES: SENATE/HOUSE-ATTENDANCE, SENATE/HOUSE-PARTY-LOYALTY
+    if (page == "senate-attendance" || page == "senate-party-loyalty" || page == "house-attendance" || page == "house-party-loyalty") {
+        //Create an array to each Party,
+        var democratsArray = [];
+        var republicantsArray = [];
+        var independentsArray = [];
+        for (let i = 0; i < membersArray.length; i++) {
+            switch (membersArray[i].party) {
+                case "D":
+                    democratsArray.push(membersArray[i]);
+                    break;
+                case "R":
+                    republicantsArray.push(membersArray[i]);
+                    break;
+                case "I":
+                    independentsArray.push(membersArray[i]);
+                    break;
+            }
         }
+
+        //Create the statistics object
+        var statistics = {
+            "number_of_democrats": democratsArray.length,
+            "number_of_republicants": republicantsArray.length,
+            "number_of_independents": independentsArray.length,
+            "number_total": membersArray.length,
+            "average_votes_with_party_pct_D": average(getValuesArrayFromObjectsArray(democratsArray, "votes_with_party_pct")),
+            "average_votes_with_party_pct_R": average(getValuesArrayFromObjectsArray(republicantsArray, "votes_with_party_pct")),
+            "average_votes_with_party_pct_I": average(getValuesArrayFromObjectsArray(independentsArray, "votes_with_party_pct")),
+            "average_votes_with_party_pct_T": average(getValuesArrayFromObjectsArray(membersArray, "votes_with_party_pct"))
+        }
+
+        //Plain table Senate/House at a place
+        //plainAtAPlace_old(statistics);
     }
 
-    //Create the statistics object
-    var statistics = {
-        "number_of_democrats": democratsArray.length,
-        "number_of_republicants": republicantsArray.length,
-        "number_of_independents": independentsArray.length,
-        "number_total": membersArray.length,
-        "average_votes_with_party_pct_D": average(getValuesArrayFromObjectsArray(democratsArray, "votes_with_party_pct")),
-        "average_votes_with_party_pct_R": average(getValuesArrayFromObjectsArray(republicantsArray, "votes_with_party_pct")),
-        "average_votes_with_party_pct_I": average(getValuesArrayFromObjectsArray(independentsArray, "votes_with_party_pct")),
-        "average_votes_with_party_pct_T": average(getValuesArrayFromObjectsArray(membersArray, "votes_with_party_pct"))
+    if (page == "senate-party-loyalty" || page == "house-party-loyalty") {
+        //Add keys to statistics object
+        statistics.members_least_loyal = statisticsLeastMost(membersArray.slice(), 10, "SmallestToHighest", "votes_with_party_pct", "total_votes");
+        statistics.members_most_loyal = statisticsLeastMost(membersArray.slice(), 10, "HighestToSmallest", "votes_with_party_pct", "total_votes");
+
+        //Plain Tbodys
+        plainTbody("tbody_sumary", plainAtAPlace(statistics));
+        plainTbody("tbody_least", statistics.members_least_loyal);
+        plainTbody("tbody_most", statistics.members_most_loyal);
+    } else if (page == "senate-attendance" || page == "house-attendance") {
+        //Add keys to statistics object
+        statistics.members_least_engaged = statisticsLeastMost(membersArray.slice(), 10, "HighestToSmallest", "missed_votes_pct", "missed_votes");
+        statistics.members_most_engaged = statisticsLeastMost(membersArray.slice(), 10, "SmallestToHighest", "missed_votes_pct", "missed_votes");
+
+        //Plain Tbodys
+        plainTbody("tbody_sumary", plainAtAPlace(statistics));
+        plainTbody("tbody_least", statistics.members_least_engaged);
+        plainTbody("tbody_most", statistics.members_most_engaged);
     }
-
-    //Plain table Senate/House at a place
-    //plainAtAPlace(statistics);
+    //FINISH PAGES: SENATE/HOUSE-ATTENDANCE, SENATE/HOUSE-PARTY-LOYALTY
 }
-
-if (page == "senate-party-loyalty" || page == "house-party-loyalty") {
-    //Add keys to statistics object
-    statistics.members_least_loyal = statisticsLeastMost(membersArray.slice(), 10, "SmallestToHighest", "votes_with_party_pct", "total_votes");
-    statistics.members_most_loyal = statisticsLeastMost(membersArray.slice(), 10, "HighestToSmallest", "votes_with_party_pct", "total_votes");
-
-    //Plain Tbodys
-    plainTbody("tbody_sumary",plainAtAPlace2(statistics));
-    plainTbody("tbody_least", statistics.members_least_loyal);
-    plainTbody("tbody_most", statistics.members_most_loyal);
-} else if (page == "senate-attendance" || page == "house-attendance") {
-    //Add keys to statistics object
-    statistics.members_least_engaged = statisticsLeastMost(membersArray.slice(), 10, "HighestToSmallest", "missed_votes_pct", "missed_votes");
-    statistics.members_most_engaged = statisticsLeastMost(membersArray.slice(), 10, "SmallestToHighest", "missed_votes_pct", "missed_votes");
-
-    //Plain Tbodys
-    plainAtAPlace(statistics);
-    plainTbody("tbody_least", statistics.members_least_engaged);
-    plainTbody("tbody_most", statistics.members_most_engaged);
-}
-//FINISH PAGES: SENATE/HOUSE-ATTENDANCE, SENATE/HOUSE-PARTY-LOYALTY
-
 
 //---------------FUNCTIONS----------------------FUNCTIONS-----------------------------FUNCTIONS--------------
 //---------------FUNCTIONS----------------------FUNCTIONS-----------------------------FUNCTIONS--------------
@@ -123,7 +156,7 @@ function moreLess(i) {
 //FINISH PAGE: INDEX
 
 //START PAGES: SENATE-DATA, HOUSE-DATA
-function createMembersTable(paramMembersArray, paramcheckBox, paramselect) {
+function createMembersTable(paramMembersArray, paramCheckBox, paramSelect) {
     //Get and clear tbody
     var tbody = document.getElementById("congress-data");
     while (tbody.firstChild) {
@@ -133,12 +166,12 @@ function createMembersTable(paramMembersArray, paramcheckBox, paramselect) {
     //Get filter parameters
     var trId = ["D", "R", "I"];
     var filterParty = [];
-    for (let i = 0; i < paramcheckBox.length; i++) {
-        if (paramcheckBox[i].checked) {
+    for (let i = 0; i < paramCheckBox.length; i++) {
+        if (paramCheckBox[i].checked) {
             filterParty.push(trId[i]);
         }
     }
-    var filterState = select[0].selectedOptions[0].value;
+    var filterState = paramSelect[0].selectedOptions[0].value;
 
     //Fill tbody
     var keysArray = [["url", "first_name", "middle_name", "last_name"], "party", "state", "seniority", "votes_with_party_pct"];
@@ -273,32 +306,32 @@ function statisticsLeastMost(array, persentage, least_most, property1, property2
     return selectedArray;
 }
 
-function plainAtAPlace(paramStatitstics) {
-    document.getElementsByClassName("cell_R_NoR")[0].textContent = paramStatitstics.number_of_republicants;
-    document.getElementsByClassName("cell_D_NoR")[0].textContent = paramStatitstics.number_of_democrats;
-    document.getElementsByClassName("cell_I_NoR")[0].textContent = paramStatitstics.number_of_independents;
-    document.getElementsByClassName("cell_T_NoR")[0].textContent = paramStatitstics.number_total;
+//function plainAtAPlace_old(paramStatitstics) {
+//    document.getElementsByClassName("cell_R_NoR")[0].textContent = paramStatitstics.number_of_republicants;
+//    document.getElementsByClassName("cell_D_NoR")[0].textContent = paramStatitstics.number_of_democrats;
+//    document.getElementsByClassName("cell_I_NoR")[0].textContent = paramStatitstics.number_of_independents;
+//    document.getElementsByClassName("cell_T_NoR")[0].textContent = paramStatitstics.number_total;
+//
+//    document.getElementsByClassName("cell_R_averageVoted")[0].textContent = paramStatitstics.average_votes_with_party_pct_R.toFixed(2) + "%";
+//    document.getElementsByClassName("cell_D_averageVoted")[0].textContent = paramStatitstics.average_votes_with_party_pct_D.toFixed(2) + "%";
+//    if (!isNaN(statistics.average_votes_with_party_pct_I)) {
+//        document.getElementsByClassName("cell_I_averageVoted")[0].textContent = paramStatitstics.average_votes_with_party_pct_I.toFixed(2) + "%";
+//    }
+//    document.getElementsByClassName("cell_T_averageVoted")[0].textContent = paramStatitstics.average_votes_with_party_pct_T.toFixed(2) + "%";
+//}
 
-    document.getElementsByClassName("cell_R_averageVoted")[0].textContent = paramStatitstics.average_votes_with_party_pct_R.toFixed(2) + "%";
-    document.getElementsByClassName("cell_D_averageVoted")[0].textContent = paramStatitstics.average_votes_with_party_pct_D.toFixed(2) + "%";
-    if (!isNaN(statistics.average_votes_with_party_pct_I)) {
-        document.getElementsByClassName("cell_I_averageVoted")[0].textContent = paramStatitstics.average_votes_with_party_pct_I.toFixed(2) + "%";
-    }
-    document.getElementsByClassName("cell_T_averageVoted")[0].textContent = paramStatitstics.average_votes_with_party_pct_T.toFixed(2) + "%";
-}
-
-function plainAtAPlace2(paramStatitstics) {
-    var rowR = ["Republican", paramStatitstics.number_of_republicants, paramStatitstics.average_votes_with_party_pct_R.toFixed(2) + "%"];
-    var rowD = ["Democrat", paramStatitstics.number_of_democrats, paramStatitstics.average_votes_with_party_pct_D.toFixed(2) + "%"];
-    var rowI = ["Independent", paramStatitstics.number_of_independents];
-    if (isNaN(statistics.average_votes_with_party_pct_I)) {
+function plainAtAPlace(paramStatistics) {
+    var rowR = ["Republican", paramStatistics.number_of_republicants, paramStatistics.average_votes_with_party_pct_R.toFixed(2) + "%"];
+    var rowD = ["Democrat", paramStatistics.number_of_democrats, paramStatistics.average_votes_with_party_pct_D.toFixed(2) + "%"];
+    var rowI = ["Independent", paramStatistics.number_of_independents];
+    if (isNaN(paramStatistics.average_votes_with_party_pct_I)) {
         rowI.push("--");
-    }else{
-        rowI.push(paramStatitstics.average_votes_with_party_pct_I.toFixed(2) + "%");
+    } else {
+        rowI.push(paramStatistics.average_votes_with_party_pct_I.toFixed(2) + "%");
     }
-    var rowT = ["Total", paramStatitstics.number_total, paramStatitstics.average_votes_with_party_pct_T.toFixed(2) + "%"];
-    
-    return [rowR,rowD,rowI,rowT]
+    var rowT = ["Total", paramStatistics.number_total, paramStatistics.average_votes_with_party_pct_T.toFixed(2) + "%"];
+
+    return [rowR, rowD, rowI, rowT]
 }
 
 function plainTbody(id, arrayOfMembers) {
